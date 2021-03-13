35eb68820b4d402dd30a399163be846579644f61
TaiwoOso <taiwo.oso10@gmail.com>
2021-03-13 15:13:41 -0500

    Finished Lab 3!

--- app/src/main/java/com/example/taiwosflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/taiwosflashcardapp/MainActivity.java
@@ -8,12 +8,30 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashcards;
+    int idx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String Q = ((TextView) findViewById(R.id.flashcard_question)).getText().toString();
+        String A = ((TextView) findViewById(R.id.flashcard_answer)).getText().toString();
+
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        flashcardDatabase.insertCard(new Flashcard(Q, A));
+        allFlashcards = flashcardDatabase.getAllCards();
+        idx = 0;
+
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
+        }
 
         TextView button = findViewById(R.id.button);
         TextView question = findViewById(R.id.flashcard_question);
@@ -21,16 +39,50 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Change Visibilities
-                question.setVisibility(View.INVISIBLE);
-                answer.setVisibility(View.VISIBLE);
+                if (button.getText().equals("Click to view answer")) {
+                    question.setVisibility(View.INVISIBLE);
+                    answer.setVisibility(View.VISIBLE);
+                    button.setText("Click to view question");
+                } else {
+                    question.setVisibility(View.VISIBLE);
+                    answer.setVisibility(View.INVISIBLE);
+                    button.setText("Click to view answer");
+                }
             }
         });
+
         findViewById(R.id.plus_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddActivity.class);
                 MainActivity.this.startActivityForResult(intent, 100);
+            }
+        });
+
+        findViewById(R.id.next_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (allFlashcards != null && idx < allFlashcards.size()) {
+                    ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(idx).getQuestion());
+                    ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(idx).getAnswer());
+                    idx = idx + 1;
+                }
+            }
+        });
+
+        findViewById(R.id.trash_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardDatabase.deleteCard(((TextView)findViewById(R.id.flashcard_question)).getText().toString());
+                allFlashcards = flashcardDatabase.getAllCards();
+                if (allFlashcards != null && allFlashcards.size() > 0) {
+                    ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
+                    ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
+                } else {
+                    ((TextView) findViewById(R.id.flashcard_question)).setText("");
+                    ((TextView) findViewById(R.id.flashcard_answer)).setText("");
+                }
+                idx = 0;
             }
         });
     }
@@ -41,10 +93,9 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == 100 && resultCode == RESULT_OK) {
             String question = data.getExtras().getString("question");
             String answer = data.getExtras().getString("answer");
-            TextView flashcard_question = findViewById(R.id.flashcard_question);
-            TextView flashcard_answer = findViewById(R.id.flashcard_answer);
-            flashcard_question.setText(question);
-            flashcard_answer.setText(answer);
+
+            flashcardDatabase.insertCard(new Flashcard(question, answer));
+            allFlashcards = flashcardDatabase.getAllCards();
 
         }
     }
